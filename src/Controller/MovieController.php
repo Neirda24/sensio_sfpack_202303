cf26eb2e1df0ef053e5fbdf25e6d70d4c6d4cc59
Adrien Roches <rochesadrien@gmail.com>
2023-03-07 10:55:22 +0100

    [01-start/05-doctrine] Moved from fake data to using entity repository

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,25 +3,27 @@
 namespace App\Controller;
 
 use App\Model\Movie;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function array_map;
 
 class MovieController extends AbstractController
 {
     #[Route('/movies', name: 'app_movies_list')]
-    public function index(): Response
+    public function index(MovieRepository $movieRepository): Response
     {
         return $this->render('movie/index.html.twig', [
-            'movies' => Movie::LIST,
+            'movies' => array_map(Movie::fromMovieEntity(...), $movieRepository->findAll()),
         ]);
     }
 
     #[Route('/movies/{slug}', name: 'app_movie_detail')]
-    public function details(string $slug): Response
+    public function details(MovieRepository $movieRepository, string $slug): Response
     {
         return $this->render('movie/details.html.twig', [
-            'movie' => Movie::getBySlug($slug),
+            'movie' => Movie::fromMovieEntity($movieRepository->getBySlug($slug)),
         ]);
     }
 }
--- src/Model/Movie.php
+++ src/Model/Movie.php
@@ -4,24 +4,33 @@ declare(strict_types=1);
 
 namespace App\Model;
 
-use function array_column;
+use App\Entity\Movie as MovieEntity;
+use DateTimeImmutable;
 
 final class Movie
 {
-    public const LIST = [
-        [
-            'title'      => 'Astérix & Obélix: Mission Cléopâtre',
-            'slug'       => 'mission-cleopatre',
-            'poster'     => 'mission-cleopatre.png',
-            'releasedAt' => '30 Jan 2002',
-            'plot'       => "Cléopâtre, la reine d’Égypte, décide, pour défier l'Empereur romain Jules César, de construire en trois mois un palais somptueux en plein désert. Si elle y parvient, celui-ci devra concéder publiquement que le peuple égyptien est le plus grand de tous les peuples. Pour ce faire, Cléopâtre fait appel à Numérobis, un architecte d'avant-garde plein d'énergie. S'il réussit, elle le couvrira d'or. S'il échoue, elle le jettera aux crocodiles.
-Celui-ci, conscient du défi à relever, cherche de l'aide auprès de son vieil ami Panoramix. Le druide fait le voyage en Égypte avec Astérix et Obélix. De son côté, Amonbofis, l'architecte officiel de Cléopâtre, jaloux que la reine ait choisi Numérobis pour construire le palais, va tout mettre en œuvre pour faire échouer son concurrent.",
-            'genres'     => ['Comedy'],
-        ],
-    ];
+    /**
+     * @param list<string> $genres
+     */
+    public function __construct(
+        public readonly string            $title,
+        public readonly string            $slug,
+        public readonly string            $poster,
+        public readonly DateTimeImmutable $releasedAt,
+        public readonly string            $plot,
+        public readonly array             $genres,
+    ) {
+    }
 
-    public static function getBySlug(string $slug): array
+    public static function fromMovieEntity(MovieEntity $movieEntity): self
     {
-        return array_column(self::LIST, null, 'slug')[$slug];
+        return new self(
+            title: $movieEntity->getTitle(),
+            slug: $movieEntity->getSlug(),
+            poster: $movieEntity->getPoster(),
+            releasedAt: $movieEntity->getReleasedAt(),
+            plot: $movieEntity->getPlot(),
+            genres: []
+        );
     }
 }
