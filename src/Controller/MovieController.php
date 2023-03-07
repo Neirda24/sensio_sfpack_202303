2721de86b7a221b3d33a7936e8603b5c4192aec1
Adrien Roches <rochesadrien@gmail.com>
2023-03-07 14:26:28 +0100

    [01-start/06-form] Create form to create / edit a movie and displays it

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,24 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie as MovieEntity;
+use App\Form\MovieType;
 use App\Model\Movie;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function array_map;
 
 class MovieController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies_list')]
-    public function index(MovieRepository $movieRepository): Response
+    #[Route('/movies', name: 'app_movies_list', methods: ['GET', 'POST'])]
+    #[Route('/movies/{slug}/edit', name: 'app_movies_edit', methods: ['GET', 'POST'])]
+    public function index(Request $request, MovieRepository $movieRepository, ?string $slug = null): Response
     {
+        $movieEntity = new MovieEntity();
+        if (null !== $slug) {
+            $movieEntity = $movieRepository->getBySlug($slug);
+        }
+
+        $createOrEditMovieForm = $this->createForm(MovieType::class, $movieEntity);
+
         return $this->render('movie/index.html.twig', [
-            'movies' => array_map(Movie::fromMovieEntity(...), $movieRepository->listAll()),
+            'movies'                => array_map(Movie::fromMovieEntity(...), $movieRepository->listAll()),
+            'createOrEditMovieForm' => $createOrEditMovieForm->createView(),
+            'edit'                  => (null !== $slug),
         ]);
     }
 
-    #[Route('/movies/{slug}', name: 'app_movie_detail')]
+    #[Route('/movies/{slug}', name: 'app_movie_detail', methods: ['GET'])]
     public function details(MovieRepository $movieRepository, string $slug): Response
     {
         return $this->render('movie/details.html.twig', [
