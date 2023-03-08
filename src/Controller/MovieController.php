140895041fb98a0cdb0b50c9f1142eeab3c2902e
Adrien Roches <rochesadrien@gmail.com>
2023-03-08 11:25:35 +0100

    [02-master/07-service] Add DI on Controllers

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -14,36 +14,41 @@ use function array_map;
 
 class MovieController extends AbstractController
 {
+    public function __construct(
+        private readonly MovieRepository $movieRepository,
+    ) {
+    }
+
     #[Route('/movies', name: 'app_movies_list', methods: ['GET', 'POST'])]
     #[Route('/movies/{slug}/edit', name: 'app_movies_edit', methods: ['GET', 'POST'])]
-    public function index(Request $request, MovieRepository $movieRepository, ?string $slug = null): Response
+    public function index(Request $request, ?string $slug = null): Response
     {
         $movieEntity = new MovieEntity();
         if (null !== $slug) {
-            $movieEntity = $movieRepository->getBySlug($slug);
+            $movieEntity = $this->movieRepository->getBySlug($slug);
         }
 
         $createOrEditMovieForm = $this->createForm(MovieType::class, $movieEntity);
         $createOrEditMovieForm->handleRequest($request);
 
         if ($createOrEditMovieForm->isSubmitted() && $createOrEditMovieForm->isValid()) {
-            $movieRepository->save($movieEntity, true);
+            $this->movieRepository->save($movieEntity, true);
 
             return $this->redirectToRoute('app_movie_detail', ['slug' => $movieEntity->getSlug()]);
         }
 
         return $this->render('movie/index.html.twig', [
-            'movies'                => array_map(Movie::fromMovieEntity(...), $movieRepository->listAll()),
+            'movies'                => array_map(Movie::fromMovieEntity(...), $this->movieRepository->listAll()),
             'createOrEditMovieForm' => $createOrEditMovieForm->createView(),
             'edit'                  => (null !== $slug),
         ]);
     }
 
     #[Route('/movies/{slug}', name: 'app_movie_detail', methods: ['GET'])]
-    public function details(MovieRepository $movieRepository, string $slug): Response
+    public function details(string $slug): Response
     {
         return $this->render('movie/details.html.twig', [
-            'movie' => Movie::fromMovieEntity($movieRepository->getBySlug($slug)),
+            'movie' => Movie::fromMovieEntity($this->movieRepository->getBySlug($slug)),
         ]);
     }
 }
